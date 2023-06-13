445abcac3bdcf86508b7c173ac1ee0f9712ae31f
vincentwong0921 <vincentwong0921@gmail.com>
2023-06-13 09:32:15 -0700

    changed

--- practice-for-week-02-pojo_basics/problems/01-keys-in-object.js
+++ practice-for-week-02-pojo_basics/problems/01-keys-in-object.js
@@ -1,5 +1,5 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
@@ -12,9 +12,28 @@ keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
-function keysInObject(obj) {
-  // Your code here
-}
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
-module.exports = keysInObject;
+
+
+
+/***********************************************************************
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
+
+
+Do this once using using a `for...in` loop and once using `Object.values`.
+
+
+Examples:
+
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
+***********************************************************************/
+
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
